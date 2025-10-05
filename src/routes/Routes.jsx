aef8d04e14d81c0941d72e3b382d5223635378b5
Nowroz Islam <116680399+nowroz@users.noreply.github.com>
2025-10-05 10:50:33 +0600

    Create routes add Root to it

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,6 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../pages/root/Root";
+
+const router = createBrowserRouter([{ path: "/", Component: Root }]);
+
+export default router;
