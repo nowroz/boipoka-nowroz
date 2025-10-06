57c557d11518ad536d55ed2e22186d59bcc24562
Nowroz Islam <116680399+nowroz@users.noreply.github.com>
2025-10-06 09:20:20 +0600

    Style the error page and show error message

--- src/pages/errorPage/ErrorPage.jsx
+++ src/pages/errorPage/ErrorPage.jsx
@@ -1,7 +1,12 @@
+import { useRouteError } from "react-router";
+
 const ErrorPage = () => {
+  const error = useRouteError();
+
   return (
-    <section>
-      <h1>Error Page</h1>
+    <section className="container mx-auto my-20 px-4 text-center">
+      <h1 className="text-7xl font-medium mb-8">Error</h1>
+      <p className="text-xl font-normal">{error.message}</p>
     </section>
   );
 };
