0ba72c37ea7c5ac60c28e41caeb579edf14f6f78
Nowroz Islam <116680399+nowroz@users.noreply.github.com>
2025-10-06 15:50:48 +0600

    Implement sorting read books list

--- src/components/readBooksList/ReadBooksList.jsx
+++ src/components/readBooksList/ReadBooksList.jsx
@@ -1,11 +1,28 @@
 import ReadBook from "../readBook/ReadBook";
 
-const ReadBooksList = ({ books, readBooks }) => {
+const ReadBooksList = ({ books, readBooks, sortType }) => {
   const onlyReadBooks = books.filter((book) => readBooks.includes(book.bookId));
+
+  const sortReadBooks = () => {
+    switch (sortType) {
+      case "Rating":
+        return onlyReadBooks.sort((a, b) => a.rating - b.rating);
+      case "Pages":
+        return onlyReadBooks.sort((a, b) => a.totalPages - b.totalPages);
+      case "Published Year":
+        return onlyReadBooks.sort(
+          (a, b) => a.yearOfPublishing - b.yearOfPublishing,
+        );
+      default:
+        return onlyReadBooks;
+    }
+  };
+
+  const sortedReadBooks = sortReadBooks();
 
   return (
     <div className="my-14 flex flex-col gap-8">
-      {onlyReadBooks.map((readBook) => (
+      {sortedReadBooks.map((readBook) => (
         <ReadBook key={readBook.bookId} readBook={readBook}></ReadBook>
       ))}
     </div>
--- src/pages/listedBooks/ListedBooks.jsx
+++ src/pages/listedBooks/ListedBooks.jsx
@@ -3,10 +3,16 @@ import "react-tabs/style/react-tabs.css";
 import ReadBooksList from "../../components/readBooksList/ReadBooksList";
 import { useLoaderData } from "react-router";
 import { getReadBooksFromLocalStorage as getReadBooks } from "../../utils/storage";
+import { useState } from "react";
 
 const ListedBooks = () => {
   const books = useLoaderData();
+  const [sortType, setSortType] = useState("");
   const readBooks = getReadBooks();
+
+  const handleSortByButtonClick = (type) => {
+    setSortType(type);
+  };
 
   return (
     <main className="container mx-auto px-4">
@@ -15,7 +21,30 @@ const ListedBooks = () => {
           Book List
         </h1>
       </section>
-      <section>
+      <section className="flex flex-col">
+        <div className="dropdown mb-10 self-center">
+          <div tabIndex={0} role="button" className="btn m-1">
+            Sort By: {sortType}
+          </div>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+          >
+            <li>
+              <a onClick={() => handleSortByButtonClick("Rating")}>Rating</a>
+            </li>
+            <li>
+              <a onClick={() => handleSortByButtonClick("Pages")}>
+                Number of Pages
+              </a>
+            </li>
+            <li>
+              <a onClick={() => handleSortByButtonClick("Published Year")}>
+                Pulbished Year
+              </a>
+            </li>
+          </ul>
+        </div>
         <Tabs>
           <TabList>
             <Tab>Read Books</Tab>
@@ -23,7 +52,11 @@ const ListedBooks = () => {
           </TabList>
 
           <TabPanel>
-            <ReadBooksList books={books} readBooks={readBooks}></ReadBooksList>
+            <ReadBooksList
+              books={books}
+              readBooks={readBooks}
+              sortType={sortType}
+            ></ReadBooksList>
           </TabPanel>
           <TabPanel>
             <h2>Any content 2</h2>
